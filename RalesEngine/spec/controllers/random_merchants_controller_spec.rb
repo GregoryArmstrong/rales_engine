45d78cfc41cc71f5832fe592ea58387585816b15
GregoryArmstrong <intergalacticgentleman@gmail.com>
2016-02-23 16:58:38 -0700

    added tests for random merchants controller. all passing.

---
+++ RalesEngine/spec/controllers/random_merchants_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::RandomMerchantsController, type: :controller do
+  fixtures :merchants
+  describe "#show" do
+    it "finds and serves one random merchants' json" do
+      get :show, format: :json
+
+      expect(response.status).to eq(200)
+      expect(response.content_type).to eq "application/json"
+
+      body = JSON.parse(response.body)
+
+      expect(body).to_not eq nil
+    end
+  end
+end
