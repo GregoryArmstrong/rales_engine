5a1ba2fc1e6a574f9a65a9671dd434be5cfc4d25
GregoryArmstrong <intergalacticgentleman@gmail.com>
2016-02-23 15:45:42 -0700

    refactored customers_controller_spec to use new fixtures

--- RalesEngine/spec/controllers/customers_controller_spec.rb
+++ RalesEngine/spec/controllers/customers_controller_spec.rb
@@ -1,19 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::CustomersController, type: :controller do
+  fixtures :customers
   describe "#index" do
     it "serves all customers' json" do
-      customer_1 = Customer.create(first_name: "first",
-                                   last_name: "last",
-                                   created_at: "2012-03-27 14:54:09 UTC",
-                                   updated_at: "2012-03-27 14:54:09 UTC"
-                                   )
-      customer_2 = Customer.create(first_name: "second",
-                                   last_name: "second last",
-                                   created_at: "2012-03-27 14:54:09 UTC",
-                                   updated_at: "2012-03-27 14:54:09 UTC"
-                                   )
-
       get :index, format: :json
 
       expect(response.status).to eq(200)
@@ -23,34 +13,28 @@ RSpec.describe Api::V1::CustomersController, type: :controller do
       first_customer = body.first
       second_customer = body.last
 
-      expect(first_customer["first_name"]).to eq customer_1.first_name
-      expect(first_customer["last_name"]).to eq customer_1.last_name
+      expect(first_customer["first_name"]).to eq customers(:customer_1).first_name
+      expect(first_customer["last_name"]).to eq customers(:customer_1).last_name
       expect(first_customer["created_at"]).to eq "2012-03-27T14:54:09.000Z"
       expect(first_customer["updated_at"]).to eq "2012-03-27T14:54:09.000Z"
-      expect(second_customer["first_name"]).to eq customer_2.first_name
-      expect(second_customer["last_name"]).to eq customer_2.last_name
-      expect(second_customer["created_at"]).to eq "2012-03-27T14:54:09.000Z"
-      expect(second_customer["updated_at"]).to eq "2012-03-27T14:54:09.000Z"
+      expect(second_customer["first_name"]).to eq customers(:customer_2).first_name
+      expect(second_customer["last_name"]).to eq customers(:customer_2).last_name
+      expect(second_customer["created_at"]).to eq "2012-03-26T14:54:09.000Z"
+      expect(second_customer["updated_at"]).to eq "2012-03-26T14:54:09.000Z"
     end
   end
 
   describe "#show" do
     it "serves a single customers' json" do
-      customer_1 = Customer.create(first_name: "first",
-                                   last_name: "last",
-                                   created_at: "2012-03-27 14:54:09 UTC",
-                                   updated_at: "2012-03-27 14:54:09 UTC"
-                                   )
-
-      get :show, format: :json, id: customer_1.id
+      get :show, format: :json, id: customers(:customer_1).id
 
       expect(response.status).to eq(200)
       expect(response.content_type).to eq "application/json"
 
       body = JSON.parse(response.body)
 
-      expect(body["first_name"]).to eq customer_1.first_name
-      expect(body["last_name"]).to eq customer_1.last_name
+      expect(body["first_name"]).to eq customers(:customer_1).first_name
+      expect(body["last_name"]).to eq customers(:customer_1).last_name
       expect(body["created_at"]).to eq "2012-03-27T14:54:09.000Z"
       expect(body["updated_at"]).to eq "2012-03-27T14:54:09.000Z"
     end
