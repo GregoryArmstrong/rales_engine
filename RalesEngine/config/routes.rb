8c6337aaf129b8f60050799a095ac97a710cf956
GregoryArmstrong <intergalacticgentleman@gmail.com>
2016-02-24 15:19:43 -0700

    revenue and revenue by date finished

--- RalesEngine/config/routes.rb
+++ RalesEngine/config/routes.rb
@@ -27,6 +27,8 @@ Rails.application.routes.draw do
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index], :controller => 'merchants/items'
         resources :invoices, only: [:index], :controller => 'merchants/invoices'
+        resources :revenue, only: [:index], :controller => 'merchants/revenue'
+        resources :favorite_customer, only: [:index], :controller => 'merchants/favorite_customer'
       end
       resources :transactions, only: [:index, :show]
       resources :customers, only: [:index, :show] do
